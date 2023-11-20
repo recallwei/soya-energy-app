970c5588c4c31a2bacb0c2f86448e5b9000a4bd1
Bruce <recall4056@gmail.com>
2023-11-20 08:58:14 +0800

    perf: :zap: add babel plugin to remove console

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'transform-remove-console', // Remove console.log
     [
       'module-resolver',
       {
